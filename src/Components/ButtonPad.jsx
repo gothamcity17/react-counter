0b6d029cc1d7aec88b7ca55dd649888402afcca6
Gowtham Katta <gowthamkatta@gmail.com>
2020-06-04 01:23:16 -0500

    Completed Counter and Updated CSS

--- src/Components/ButtonPad.jsx
+++ src/Components/ButtonPad.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-export default function ButtonPad() {
-    return (
-        <div>
-            <button>Up Button</button>
-            <button>Down Button</button>
-        </div>
-    )
+export default function ButtonPad({
+  Increment_5,
+  Increment_1,
+  Reset,
+  Decrement_1,
+  Decrement_5,
+}) {
+  return (
+    <div>
+      <button className='number' onClick={Increment_5}>
+        {' '}
+        +5{' '}
+      </button>
+      <button className='number' onClick={Increment_1}>
+        {' '}
+        +1{' '}
+      </button>
+      <button className='reset' onClick={Reset}>
+        {' '}
+        Reset{' '}
+      </button>
+      <button className='number' onClick={Decrement_1}>
+        {' '}
+        -1{' '}
+      </button>
+      <button className='number' onClick={Decrement_5}>
+        {' '}
+        -5{' '}
+      </button>
+    </div>
+  );
 }
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPad from './ButtonPad';
 
-
 function Counter() {
-    return (
-        <div className="counter">
-            <Display/>
-            <ButtonPad/>
-        </div>
-    )
+  const [count, setCount] = useState(0);
+
+  let Increment_5 = () => setCount(count + 5);
+
+  let Increment_1 = () => setCount(count + 1);
+
+  let Reset = () => setCount(0);
+
+  let Decrement_1 = () => setCount(count - 1);
+
+  let Decrement_5 = () => setCount(count - 5);
+
+  return (
+    <div className='counter'>
+      <Display display={count} />
+      <ButtonPad
+        Increment_5={Increment_5}
+        Increment_1={Increment_1}
+        Reset={Reset}
+        Decrement_1={Decrement_1}
+        Decrement_5={Decrement_5}
+      />
+    </div>
+  );
 }
 
 export default Counter;
